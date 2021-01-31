149a5b290f78f7e770dc7b3ad85aef9f2e0e51f7
Akash Dasgupta <akashdasgupta@physics.ox.ac.uk>
2021-01-31 23:11:47 +0000

    Added function to find what data was collected

--- basic_funcs.py
+++ basic_funcs.py
@@ -1,1 +1,20 @@
 from external_imports import *
+
+def path_process(path):
+    db = {}  
+
+    for root, dirs, _ in os.walk(path):
+        for dir in dirs:
+            db[dir] = []
+            for _, subdirs, _ in os.walk(root+"\\"+dir):
+                for subdir in subdirs:
+                    try:
+                        float(subdir)
+                        db[dir].append(subdir)
+                    except ValueError:
+                        pass
+                break
+        break
+    return db
+
+
