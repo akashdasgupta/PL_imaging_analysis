0150368787e05a9ea0edd8a8e93e6e1ddbcb9f4d
Akash Dasgupta <akashdasgupta@physics.ox.ac.uk>
2021-02-23 23:07:47 +0000

    Added white cal funcs, modified some stuff

--- image_process.py
+++ image_process.py
@@ -1,4 +1,5 @@
 from external_imports import *
+from iv_parser import *
 
 def GetNominalV(path):
     nominal_v = []
@@ -10,19 +11,18 @@ def GetNominalV(path):
             corosponding_I.append(float(row[1]))
     return nominal_v, corosponding_I
 
-def averager(paths):
-    temp = np.array(Image.open(paths[0]))
+def averager(basepath, filenames):
+    temp = np.array(Image.open(basepath+'\\'+filenames[0]))
     rows, cols = temp.shape
     del temp
 
     buffer = np.zeros((rows, cols))
-    for path in paths:
-        im = np.array(Image.open(path), dtype=np.float32) # So there's enough headroom for the uint16
+    for filename in filenames:
+        im = np.array(Image.open(basepath+'\\'+filename), dtype=np.float32) # So there's enough headroom for the uint16
         buffer += im
         del im # unnescesary but just in case
-    average_arr = buffer / len(paths)
+    average_arr = buffer / len(basepath+'\\'+filename)
     del buffer # unnescesary but just in case
-    average_arr = average_arr.astype("uint16")
     return average_arr
 
 def find_tif(datapath):
@@ -30,7 +30,8 @@ def find_tif(datapath):
     for _, _, files in os.walk(datapath):
         for file in files:
             if file.endswith(".tif"): # Only interested in tifs
-                raw_paths.append(datapath + '/'+file)
+                raw_paths.append(file)
+        break
     return raw_paths
 
 def white_mask_maker(imarr):
@@ -65,9 +66,33 @@ def white_mask_maker(imarr):
     
     return (rmin,rmax,cmin,cmax)
 
-def calibrate(imarr, wimarr):
-    return (imarr/wimarr )*np.mean(wimarr)
+def white_nonunif(imarr, center_row, center_col, diam):
+    pix_in_powermeter = []
+    for i in range(imarr.shape[0]):
+        for j in range(imarr.shape[1]):
+            if( (i-center_row)**2 + (j-center_col)**2) <= (diam/2)**2:
+                pix_in_powermeter.append(imarr[i,j])
+    cal_arr = deepcopy(imarr)
+    return cal_arr/ np.mean(pix_in_powermeter)
 
+white_buffer=[None, None, None, None]
+def callback_return_size(image_name, shape):
+    colstart, rowstart, width, height = shape.size
+    rowend = rowstart+height
+    colend = colstart+width
+    
+    white_buffer[0] = rowstart
+    white_buffer[1] = rowend
+    white_buffer[2] = colstart
+    white_buffer[3] = colend
 
-
-
+pix_buffer=[None, None, None, None]
+def callback_return_pix_size(image_name, shape):
+    colstart, rowstart, width, height = shape.size
+    rowend = rowstart+height
+    colend = colstart+width
+    
+    pix_buffer[0] = rowstart
+    pix_buffer[1] = rowend
+    pix_buffer[2] = colstart
+    pix_buffer[3] = colend
