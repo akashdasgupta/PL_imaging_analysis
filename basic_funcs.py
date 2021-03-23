70541567f615d9bee6340b64bbb269dc4d0ce588
Akash Dasgupta <akashdasgupta@physics.ox.ac.uk>
2021-03-23 20:27:15 +0000

    Formatting changes

--- basic_funcs.py
+++ basic_funcs.py
@@ -6,7 +6,7 @@ def path_process(path):
     for root, dirs, _ in os.walk(path):
         for dir in dirs:
             db[dir] = []
-            for _, subdirs, _ in os.walk(root+"\\"+dir):
+            for _, subdirs, _ in os.walk(f"{root}\\{dir}"):
                 for subdir in subdirs:
                     try:
                         int(subdir)
@@ -26,23 +26,23 @@ def find_tif(datapath):
         break
     return raw_paths
 
-def image_for_cropping(db, basepath):
-    images = []
-    names = []
-    corosponding_paths = []
-    for key in db.keys():
-        for pix in db[key]:
-            image_names = find_tif(basepath+'\\'+key+'\\'+str(pix)+'\\camera')
-            nominal_vs = [i.split('_')[1].split('=')[1] for i in image_names]
-            max_filename = image_names[np.argmax(nominal_vs)]
-            im = np.array(Image.open(basepath+'\\'+key+'\\'+str(pix)+'\\camera\\'+max_filename))
-            im_scaled = im *(2**(8)/np.max(im)) # 8bit 
-            name = key+', Pixel '+str(pix)
+# def image_for_cropping(db, basepath):
+#     images = []
+#     names = []
+#     corosponding_paths = []
+#     for key in db.keys():
+#         for pix in db[key]:
+#             image_names = find_tif(basepath+'\\'+key+'\\'+str(pix)+'\\camera')
+#             nominal_vs = [i.split('_')[1].split('=')[1] for i in image_names]
+#             max_filename = image_names[np.argmax(nominal_vs)]
+#             im = np.array(Image.open(basepath+'\\'+key+'\\'+str(pix)+'\\camera\\'+max_filename))
+#             im_scaled = im *(2**(8)/np.max(im)) # 8bit 
+#             name = key+', Pixel '+str(pix)
 
-            images.append(im_scaled.astype(np.uint8))
-            names.append(name)
-            corosponding_paths.append(basepath+'\\'+key+'\\'+str(pix)+'\\camera')
-    return images, names, corosponding_paths
+#             images.append(im_scaled.astype(np.uint8))
+#             names.append(name)
+#             corosponding_paths.append(basepath+'\\'+key+'\\'+str(pix)+'\\camera')
+#     return images, names, corosponding_paths
 
 
 
