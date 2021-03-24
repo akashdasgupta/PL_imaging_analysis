5400778f1fb631f641288cd22471196510d71f8b
Akash Dasgupta <akashdasgupta@physics.ox.ac.uk>
2021-03-24 23:27:26 +0000

    just added some more funcs, moved some in here

--- basic_funcs.py
+++ basic_funcs.py
@@ -5,15 +5,16 @@ def path_process(path):
     db = {}  
     for root, dirs, _ in os.walk(path):
         for dir in dirs:
-            db[dir] = []
-            for _, subdirs, _ in os.walk(f"{root}\\{dir}"):
-                for subdir in subdirs:
-                    try:
-                        int(subdir)
-                        db[dir].append(int(subdir))
-                    except ValueError:
-                        pass
-                break
+            if dir.lower() != 'white':
+                db[dir] = []
+                for _, subdirs, _ in os.walk(f"{root}\\{dir}"):
+                    for subdir in subdirs:
+                        try:
+                            int(subdir)
+                            db[dir].append(int(subdir))
+                        except ValueError:
+                            pass
+                    break
         break
     return db
 
@@ -26,23 +27,38 @@ def find_tif(datapath):
         break
     return raw_paths
 
-# def image_for_cropping(db, basepath):
-#     images = []
-#     names = []
-#     corosponding_paths = []
-#     for key in db.keys():
-#         for pix in db[key]:
-#             image_names = find_tif(basepath+'\\'+key+'\\'+str(pix)+'\\camera')
-#             nominal_vs = [i.split('_')[1].split('=')[1] for i in image_names]
-#             max_filename = image_names[np.argmax(nominal_vs)]
-#             im = np.array(Image.open(basepath+'\\'+key+'\\'+str(pix)+'\\camera\\'+max_filename))
-#             im_scaled = im *(2**(8)/np.max(im)) # 8bit 
-#             name = key+', Pixel '+str(pix)
+def get_cam_exposure(path):
+    with open(path+'\\camera\\camera_setting_dump.txt') as file:
+        for line in file:
+            db = eval(line)
+            db = eval(line)
+            return float(db['ExposureTime'])
 
-#             images.append(im_scaled.astype(np.uint8))
-#             names.append(name)
-#             corosponding_paths.append(basepath+'\\'+key+'\\'+str(pix)+'\\camera')
-#     return images, names, corosponding_paths
+def image_intsweep_name_parser(path):
+    tif_names = find_tif(path)
+    repeat_db = {}
+
+    for tif_name in tif_names:
+        sm_output = tif_name.split('_')[0]
+        led_v = tif_name.split('_')[1].split('=')[1]
+
+        if f"{sm_output}_{led_v}" in repeat_db.keys():
+            repeat_db[f"{sm_output}_{led_v}"].append(tif_name)
+        else:
+            repeat_db[f"{sm_output}_{led_v}"] = [tif_name]
+    return repeat_db
+
+
+def image_for_cropping(path):
+    image_names = find_tif(path)
+
+    nominal_vs = [i.split('_')[1].split('=')[1] for i in image_names]
+    max_filename = image_names[np.argmax(nominal_vs)]
+
+    im = np.array(Image.open(f"{path}\\{max_filename}"))
+    im_scaled = im *(2**(8)/np.max(im)) # 8bit 
+    image= im_scaled.astype(np.uint8)
+    return image
 
 
 
