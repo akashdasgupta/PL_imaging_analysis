ffa9c92728a22179044ffae311f1d27283dbbe7b
Akash Dasgupta <akashdasgupta@physics.ox.ac.uk>
2021-04-07 12:26:30 +0100

    Commented out the jsc difference

--- reconstruct_jv.py
+++ reconstruct_jv.py
@@ -4,7 +4,7 @@ from image_process import *
 def single_pixel_jv(path, row, col, flux_1sun, voc_rad, white_mean_scaled):
     # Lists to hold data
     dp_oc = []
-    dp_sc = []
+    # dp_sc = []
     white_refs = []
     fluxes = []
     # For Voc_rad calculatuion:
@@ -15,7 +15,7 @@ def single_pixel_jv(path, row, col, flux_1sun, voc_rad, white_mean_scaled):
 
     for filename_voc in filenames_voc:
         # Asuming there is a sc file for every oc file, and it's saved by the same name format:
-        filename_isc = 'SC_'+'_'.join(filename_voc.split('_')[1:])
+        # filename_isc = 'SC_'+'_'.join(filename_voc.split('_')[1:])
         flux = float(filename_voc.split('_')[1])
         exposure = float(filename_voc.split('_')[2])
         fluxes.append(flux)
@@ -24,22 +24,22 @@ def single_pixel_jv(path, row, col, flux_1sun, voc_rad, white_mean_scaled):
 
         datapoint_voc =  np.mean(np.load(f"{path}\\oc\\{filename_voc}")[row-ave_rad:row+ave_rad,
                                                                        col-ave_rad:col+ave_rad])
-        datapoint_isc =  np.mean(np.load(f"{path}\\sc\\{filename_isc}")[row-ave_rad:row+ave_rad,
-                                                                       col-ave_rad:col+ave_rad])
+        # datapoint_isc =  np.mean(np.load(f"{path}\\sc\\{filename_isc}")[row-ave_rad:row+ave_rad,
+        #                                                                col-ave_rad:col+ave_rad])
         
         dp_oc.append(datapoint_voc/exposure)
-        dp_sc.append(datapoint_isc/exposure)
+        # dp_sc.append(datapoint_isc/exposure)
         white_refs.append(white_mean_scaled*flux)
 
     dp_oc = np.array(dp_oc)
-    dp_sc = np.array(dp_sc)
+    # dp_sc = np.array(dp_sc)
     white_refs = np.array(white_refs)
     
     # Calculate number of suns:
     num_suns = np.array(fluxes) / flux_1sun
     voc_rads = (sci.k*298/sci.e)*np.log((jsc_by_j0*num_suns)+1)
 
-    rrs = (dp_oc-dp_sc)/(white_refs)
+    rrs = (dp_oc)/(white_refs)
     Vs =  voc_rads +  (sci.k*298/sci.e)*np.log(rrs)
     Js = (1-num_suns)
     
@@ -49,7 +49,7 @@ def single_pixel_jv(path, row, col, flux_1sun, voc_rad, white_mean_scaled):
 def whole_image_jv(path, flux_1sun, voc_rad, white_mean_scaled):
     # Lists to hold data
     dp_oc = []
-    dp_sc = []
+    # dp_sc = []
     white_refs = []
     fluxes = []
     # For Voc_rad calculatuion:
@@ -60,62 +60,64 @@ def whole_image_jv(path, flux_1sun, voc_rad, white_mean_scaled):
 
     for filename_voc in filenames_voc:
         # Asuming there is a sc file for every oc file, and it's saved by the same name format:
-        filename_isc = 'SC_'+'_'.join(filename_voc.split('_')[1:])
+        # filename_isc = 'SC_'+'_'.join(filename_voc.split('_')[1:])
         flux = float(filename_voc.split('_')[1])
         exposure = float(filename_voc.split('_')[2])
         fluxes.append(flux)
 
         datapoint_voc =  np.mean(np.load(f"{path}\\oc\\{filename_voc}"))
-        datapoint_isc =  np.mean(np.load(f"{path}\\sc\\{filename_isc}"))
+        # datapoint_isc =  np.mean(np.load(f"{path}\\sc\\{filename_isc}"))
         
         dp_oc.append(datapoint_voc/exposure)
-        dp_sc.append(datapoint_isc/exposure)
+        # dp_sc.append(datapoint_isc/exposure)
         white_refs.append(white_mean_scaled*flux)
 
     dp_oc = np.array(dp_oc)
-    dp_sc = np.array(dp_sc)
+    # dp_sc = np.array(dp_sc)
     white_refs = np.array(white_refs)
     
     # Calculate number of suns:
     num_suns = np.array(fluxes) / flux_1sun
     voc_rads = (sci.k*298/sci.e)*np.log((jsc_by_j0*num_suns)+1)
 
-    rrs = (dp_oc-dp_sc)/(white_refs)
+    rrs = (dp_oc)/(white_refs)
     Vs =  voc_rads +  (sci.k*298/sci.e)*np.log(rrs)
     Js = (1-num_suns)
     
     return num_suns, rrs, Vs, Js, fluxes
 
 
-def array_jv(path, flux_1sun, voc_rad, white_mean_scaled):
+def array_jv(path, savepath, flux_1sun, voc_rad, white_mean_scaled):
     # For Voc_rad calculatuion:
     jsc_by_j0 = np.exp(sci.e*voc_rad/(sci.k*298))-1
         
     filenames_voc = find_npy(f"{path}\\oc") 
 
+    if not os.path.isdir(f"{savepath}\\PLQE"):
+        os.makedirs(f"{savepath}\\PLQE")
+    if not os.path.isdir(f"{savepath}\\QFLS"):
+        os.makedirs(f"{savepath}\\QFLS")
+
     for filename_voc in filenames_voc:
         # Asuming there is a sc file for every oc file, and it's saved by the same name format:
-        filename_isc = 'SC_'+'_'.join(filename_voc.split('_')[1:])
         flux = float(filename_voc.split('_')[1])
         exposure = float(filename_voc.split('_')[2])
 
-        im_voc =  np.load(f"{path}\\oc\\{filename_voc}")
-        im_isc =  np.load(f"{path}\\sc\\{filename_isc}")
+        im_voc =  np.load(f"{path}\\oc\\{filename_voc}")/exposure
+        # im_isc =  np.load(f"{path}\\sc\\{filename_isc}")/exposure
         
         white_ref = white_mean_scaled*flux 
 
         num_sun = flux/flux_1sun
+        voc_rad = (sci.k*298/sci.e)*np.log((jsc_by_j0*num_sun)+1)
 
-    # Calculate number of suns:
-    white_nonunif = np.load(f"{path}\\white.npy")
-    num_suns = np.array(fluxes) / flux_1sun
-    voc_rads = (sci.k*298/sci.e)*np.log((jsc_by_j0*num_suns)+1)
+        rr = (im_voc)/(white_ref)
+        V =  voc_rad +  (sci.k*298/sci.e)*np.log(rr)
+        # convention: [num suns]_[J]_[flux]_.npy
+        filename = f"{num_sun}_{1-num_sun}_{flux}_"
 
-    rrs = (dp_oc-dp_sc)/(white_refs)
-    Vs =  voc_rads +  (sci.k*298/sci.e)*np.log(rrs)
-    Js = (1-num_suns)
-    
-    return num_suns, rrs, Vs, Js, fluxes
+        np.save(f"{savepath}\\PLQE\\PLQE_{filename}", rr)
+        np.save(f"{savepath}\\V\\V{filename}", V)
 
 
 
