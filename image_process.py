5613aaaba30f54860227e999bbce6ed07dc4740b
Akash Dasgupta <akashdasgupta@physics.ox.ac.uk>
2021-08-27 10:21:48 +0100

    added parallel processing of file in PLQE

--- image_process.py
+++ image_process.py
@@ -4,6 +4,11 @@ from interactivecrop.interactivecrop import main as crop # Cropping window
 from copy import deepcopy # So we don't screw up raw arrays that we act on
 from scipy.ndimage import gaussian_filter
 
+# Parallel_processing
+from joblib import Parallel, delayed
+import multiprocessing
+num_cores = multiprocessing.cpu_count()
+
 from external_imports import *
 from basic_funcs import *
 
@@ -114,10 +119,11 @@ def save_PLQE(datapath, savepath, whitefilepath, whiteparamsfilepath, bandgap, f
     if not os.path.isdir(f"{savepath}\\PLQE_{savename}"):
         os.makedirs(f"{savepath}\\PLQE_{savename}")
     filenames = find_npy(datapath)
-    for filename in filenames:
+    def process_one_file(filename):
         bias, num_sun, flux, PLQE =  PLQEmap(datapath,filename, whitefilepath, whiteparamsfilepath, bandgap)
         savefilename = f"{bias}_{num_sun}_{flux}_"
-        np.save(f"{savepath}\\PLQE_{savename}\\{savefilename}")
+        np.save(f"{savepath}\\PLQE_{savename}\\{savefilename}", PLQE)
+    Parallel(n_jobs=num_cores)(delayed(process_one_file)(filename) for filename in filenames)
 
 white_buffer=[None, None, None, None]
 def callback_return_size(image_name, shape):
