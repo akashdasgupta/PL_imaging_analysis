8fbe400b9e10836dbe6673297983359c092ea8f3
Akash Dasgupta <akashdasgupta@physics.ox.ac.uk>
2021-09-01 11:15:38 +0100

    fixed bugs with both funcs

--- voltage_dependant.py
+++ voltage_dependant.py
@@ -34,55 +34,57 @@ def vsweep_array_QFLS(path, voc_rad0):
 
 def vsweep_col_eff(path, rawpath):
     for direction in ["vsweep_f", "vsweep_b", "vsweep"]:
-        if not os.path.isdir(f"{path}\\{direction}_coleff"):
-            os.makedirs(f"{path}\\{direction}_coleff")
-        
-        # Figure out if we have enough points for a OC image from this dataset
-        Vs = []
-        Is = []
-        with open(f"{rawpath}\\{direction}\\source_meter.csv", 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                Vs.append(float(row[0]))
-                Is.append(float(row[1]))
-        
-        sign0 = Is[0]/abs[Is[0]]
-        oc_extrapolate = False
-        for i in Is:
-            if i/ abs(i) != sign0:
-                oc_extrapolate = True
-                V_above = Vs[i-1]
-                I_above = Is[i-1]
-                V_bellow = Vs[i]
-                I_bellow = Is[i]
-                break
-
-        if oc_extrapolate:
-            filenames = find_npy(f"{path}\\PLQE_{direction}") 
-            for filename in filenames:
-                if float(filename.split('_')[0].split('=')[1]) == np.around(V_above,3):
-                    PLQE_oc_above = np.load(f"{path}\\PLQE_{direction}\\{filename}")
-                elif float(filename.split('_')[0].split('=')[1]) == np.around(V_bellow, 3):
-                    PLQE_oc_bellow = np.load(f"{path}\\PLQE_{direction}\\{filename}")
+        if os.path.isdir(f"{rawpath}\\{direction}"):
+            if not os.path.isdir(f"{path}\\coleff_{direction}"):
+                os.makedirs(f"{path}\\coleff_{direction}")
             
+            # Figure out if we have enough points for a OC image from this dataset
+            Vs = []
+            Is = []
+            with open(f"{rawpath}\\{direction}\\source_meter.csv", 'r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    Vs.append(float(row[0]))
+                    Is.append(float(row[1]))
+            
+            sign0 = Is[0]/abs(Is[0])
+            oc_extrapolate = False
+            for i,I in enumerate(Is):
+                if I/abs(I) != sign0:
+                    oc_extrapolate = True
+                    V_above = Vs[i-1]
+                    I_above = Is[i-1]
+                    V_bellow = Vs[i]
+                    I_bellow = Is[i]
+                    break
+
+            if oc_extrapolate:
+                filenames = find_npy(f"{path}\\PLQE_{direction}") 
+                for filename in filenames:
+                    if float(filename.split('_')[0]) == np.around(V_above,3):
+                        PLQE_oc_above = np.load(f"{path}\\PLQE_{direction}\\{filename}")
+                    elif float(filename.split('_')[0]) == np.around(V_bellow, 3):
+                        PLQE_oc_bellow = np.load(f"{path}\\PLQE_{direction}\\{filename}")
+                    
                 m_ocarr = (PLQE_oc_bellow - PLQE_oc_above)/ (I_bellow-I_above)
-                PLQE_oc = m_ocarr*I_bellow  # I=0, so oc image = c
-        
-        else:
-            PLQE_oc = oc_image_maker(f"{path}\\PLQE_oc",f"{path}\\{direction}")
-            if not PLQE_oc:
-                raise FileNotFoundError("Couldn't find enough files for OC image")
-        
-        for filename in filenames:
-            bias = float(filename.split('_')[0].split('=')[1])
-            num_sun = float(filename.split('_')[1])
-            flux = float(filename.split('_')[2])
-            savename = f"{bias}_{num_sun}_{flux}_"
+                c = PLQE_oc_bellow - m_ocarr*I_bellow
+                PLQE_oc = c  # I=0, so oc image = c
+                
+            
+            else:
+                PLQE_oc = oc_image_maker(f"{path}\\PLQE_oc",f"{path}\\{direction}")
+                if not PLQE_oc:
+                    raise FileNotFoundError("Couldn't find enough files for OC image")
+            
+            for filename in filenames:
+                bias = float(filename.split('_')[0])
+                num_sun = float(filename.split('_')[1])
+                flux = float(filename.split('_')[2])
+                savename = f"{bias}_{num_sun}_{flux}_"
 
-            PLQE_v =  np.load(f"{path}\\PLQE_{direction}\\{filename}")
-            col_eff = (PLQE_oc-PLQE_v) / PLQE_oc
-            np.save(f"{path}\\{direction}_coleff\\{savename}", col_eff)
-
+                PLQE_v =  np.load(f"{path}\\PLQE_{direction}\\{filename}")
+                col_eff = (PLQE_oc-PLQE_v) / PLQE_oc
+                np.save(f"{path}\\coleff_{direction}\\{savename}", col_eff)
 
 def oc_image_maker(oc_path, vsweep_path):
     oc_filenames = find_npy(oc_path)
