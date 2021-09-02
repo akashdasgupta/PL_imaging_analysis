0761e9f9dd1bae84eda775c2e265f7d27f4bbd6b
Akash Dasgupta <akashdasgupta@physics.ox.ac.uk>
2021-09-02 09:59:18 +0100

    adeede new trial functions for getting Jsc maps

--- intensity_dependant.py
+++ intensity_dependant.py
@@ -223,3 +223,73 @@ def get_idelity_map(path):
     os.remove('temp.npy')
     return n_id_final
 
+def get_J0_map(path):
+    filenames = find_npy(f"{path}\\QFLS_int_oc")
+    flux = np.array([float(i.split('_')[2]) for i in filenames])
+    JGs =  flux * (sci.e*1e3) # in mA
+    QFLS_arrs = [np.load(f"{path}\\QFLS_int_oc\\{filename}") for filename in filenames]
+    
+    plt.scatter(np.exp((sci.e*np.array([np.mean(i) for i in QFLS_arrs]))/(sci.k * 298)), JGs)
+    plt.show()
+
+    J0 = np.zeros(QFLS_arrs[0].shape)
+    # Must save so that we can memmap arr:
+    np.save('temp', J0)
+    def process_row(i):
+        # Opens arrat:
+        arr = np.load('temp.npy', mmap_mode='r+')
+        for j in range(QFLS_arrs[0].shape[1]):
+            QFLS_pp = []
+            for QFLS_arr in QFLS_arrs:
+                QFLS_pp.append(QFLS_arr[i,j])
+            
+            QFLS_pp = np.array(QFLS_pp)
+            exp_term = np.exp((sci.e*QFLS_pp)/(sci.k * 298))     
+            exp_term = [i for _,i in sorted(zip(JGs,exp_term))]
+            JGs.sort()
+            
+            for kp in range(len(JGs)-2):
+                k=kp+1
+                m,c = stats.linregress(exp_term[0:k], JGs[0:k])[0:2]
+                grad_vec = np.array([exp_term[k+1]-exp_term[k+1], JGs[k] +  m*(exp_term[k+1]-exp_term[k+1])])
+                next_vector = np.array([exp_term[k+1]-exp_term[k+1], JGs[k+1]])
+                dp = np.dot(grad_vec,next_vector)
+                theta = np.arccos(dp/(np.sqrt(np.dot(grad_vec,grad_vec))*np.sqrt(np.dot(next_vector,next_vector))))
+                if theta * 180/np.pi >= 2.5:
+                    break
+            arr[i,j] = m
+
+
+    Parallel(n_jobs=num_cores, verbose = 0)(delayed(process_row)(i) for i in range(QFLS_arrs[0].shape[0]))
+    J0_final = np.load('temp.npy')
+    # Delete temp: 
+    os.remove('temp.npy')
+    return J0_final
+
+def get_Jsc_map(path):
+    J0 = np.load(f"{path}\\J0.npy")
+
+    ### Get sc image at 1 sun
+    filenames = find_npy(f"{path}\\QFLS_int_sc")
+    num_suns = np.array([float(i.split('_')[1]) for i in filenames])
+
+    filenames = [i for _,i in sorted(zip(num_suns, filenames))]
+    num_suns.sort()
+
+    sign_0 = (num_suns[0] - 1)/abs(num_suns[0] - 1)
+    for i,num_sun in enumerate(num_suns):
+        if (num_sun-1)/abs(num_sun- 1) != sign_0:
+            num_sun_above = num_sun
+            arr_sc_above = np.load(f"{path}\\QFLS_int_sc\\{filenames[i]}")
+            num_sun_bellow = num_suns[i-1]
+            arr_sc_bellow = np.load(f"{path}\\QFLS_int_sc\\{filenames[i-1]}")
+    
+    m_arr = (arr_sc_above - arr_sc_bellow)/(num_sun_above-num_sun_bellow)
+    c_arr = arr_sc_above - m_arr*num_sun_above
+    QFLS_sc = m_arr + c_arr 
+    ###
+
+    JG = (float(filenames[0].split('_')[2]) /float(filenames[0].split('_')[1]))*(sci.e*1000)
+    J_loss = J0 * np.exp((sci.e*QFLS_sc)/(sci.k * 298))
+    J_sc = JG - J_loss
+    return J_sc, J_loss
