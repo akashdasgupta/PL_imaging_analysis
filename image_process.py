57076aeeee07975370ffaef628751000fe968120
Akash Dasgupta <akashdg09@gmail.com>
2022-02-20 14:52:40 +0000

    Seperated external PLQE as its own function

--- image_process.py
+++ image_process.py
@@ -75,7 +75,7 @@ def white_over_cell_correction(led_specf, cell_specf, bandgap, camQEf, lenscalf,
     return white_factor/cell_factor
 
 
-def PLQEmap(datapath, filename, whitefilepath, whiteparamsfilepath, bandgap, filterODf, flux_1sun=None, external_PLQE=None, one_sun_flux_pull_path=None):
+def PLQEmap(datapath, filename, whitefilepath, whiteparamsfilepath, bandgap, filterODf, flux_1sun=None, one_sun_flux_pull_path=None):
     bias = filename.split('_')[0]
     if bias.lower() != 'oc' and  bias.lower() != 'sc':
         bias = float(bias.split('=')[1])
@@ -91,41 +91,36 @@ def PLQEmap(datapath, filename, whitefilepath, whiteparamsfilepath, bandgap, fil
     print(flux_1sun)
     num_sun = flux/flux_1sun
 
-
-    if external_PLQE:
-        im_cell =  np.load(f"{datapath}/{filename}")
-        PLQE = (im_cell/np.mean(im_cell) )* external_PLQE
-    else: 
            
-        # Load the white params: 
-        white_exposure = None
-        white_flux = None
+    # Load the white params: 
+    white_exposure = None
+    white_flux = None
 
-        with open(whiteparamsfilepath,'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if row[0] == 'Exposure':
-                    white_exposure = float(row[1])
-                if row[0] == 'Flux scale':
-                    white_flux = float(row[1])
+    with open(whiteparamsfilepath,'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == 'Exposure':
+                white_exposure = float(row[1])
+            if row[0] == 'Flux scale':
+                white_flux = float(row[1])
 
-        if white_exposure == None or white_flux == None:
-            raise ValueError("Couldn't find white params!")
+    if white_exposure == None or white_flux == None:
+        raise ValueError("Couldn't find white params!")
 
-        # calculate correction
-        correction = white_over_cell_correction(ledspecf, np.vectorize(BBf_cellf), 
-                                                    bandgap, camqef, lenscalf, 0.99, filterODf)
+    # calculate correction
+    correction = white_over_cell_correction(ledspecf, np.vectorize(BBf_cellf), 
+                                                bandgap, camqef, lenscalf, 0.99, filterODf)
 
-        # load white
-        white_mean = np.mean(np.load(whitefilepath)) / white_exposure
+    # load white
+    white_mean = np.mean(np.load(whitefilepath)) / white_exposure
 
-        # Extract params from filename
-        exposure = float(filename.split('_')[2])
+    # Extract params from filename
+    exposure = float(filename.split('_')[2])
 
 
-        # Main calculation
-        im_cell =  np.load(f"{datapath}/{filename}")/exposure
-        PLQE = (im_cell/white_mean) * correction * (white_flux/flux)
+    # Main calculation
+    im_cell =  np.load(f"{datapath}/{filename}")/exposure
+    PLQE = (im_cell/white_mean) * correction * (white_flux/flux)
 
     return bias, num_sun, flux, PLQE
 
@@ -133,27 +128,67 @@ def save_PLQE(datapath, savepath, whitefilepath, whiteparamsfilepath, bandgap, f
     if not os.path.isdir(f"{savepath}/PLQE_{savename}"):
         os.makedirs(f"{savepath}/PLQE_{savename}")
     filenames = find_npy(datapath)
-
-    if os.path.isfile(f"{datapath}/plqe_ext.txt"):
-        with open(f"{datapath}/plqe_ext.txt",'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                PLQE_ext = float(row[0])
-                break
-    else:
-        PLQE_ext=None
-        print('No ext for: ', datapath)
-    print('I got herer!')
-    # def process_one_file(filename):
-    #     bias, num_sun, flux, PLQE =  PLQEmap(datapath,filename, whitefilepath, whiteparamsfilepath, bandgap, filterODf, flux_1sun=flux_1sun)
-    #     savefilename = f"{bias}_{num_sun}_{flux}_"
-    #     np.save(f"{savepath}/PLQE_{savename}/{savefilename}", PLQE)
-    for filename in filenames:
-        bias, num_sun, flux, PLQE =  PLQEmap(datapath,filename, whitefilepath, whiteparamsfilepath, bandgap, filterODf, flux_1sun=flux_1sun,external_PLQE=PLQE_ext, one_sun_flux_pull_path=one_sun_flux_pull_path)
+    def process_one_file(filename):
+        bias, num_sun, flux, PLQE =  PLQEmap(datapath,filename, whitefilepath, whiteparamsfilepath, bandgap, filterODf, flux_1sun=flux_1sun,one_sun_flux_pull_path=one_sun_flux_pull_path)
         savefilename = f"{bias}_{num_sun}_{flux}_"
         np.save(f"{savepath}/PLQE_{savename}/{savefilename}", PLQE)
     
-    # Parallel(n_jobs=num_cores)(delayed(process_one_file)(filename) for filename in filenames)
+    Parallel(n_jobs=num_cores)(delayed(process_one_file)(filename) for filename in filenames)
+
+def external_PLQE_averager(datapath, savepath):
+
+    path_db = path_process(datapath)
+    for key in path_db.keys():
+
+        for pix in path_db[key]:
+            for _,biases,_ in os.walk(f"{datapath}/{key}/{pix}"):
+                break
+            break
+        
+        for bias in biases:
+            if bias.lower() == 'oc' or bias.lower() == 'sc':
+                 # We assume the file holds the 1 sun value
+                with open(f"{datapath}/{key}/PLQE_EXT/{bias}/plqe_ext.txt", 'r') as file:
+                    reader = csv.reader(file)
+                    for row in reader:
+                        ext_PLQE_1sun = float(row[0])
+                        break
+            pix_ave_plqe = {}
+            for pix in path_db[key]:
+                filenames = find_npy(f"{savepath}/{key}/{pix}/PLQE_{bias}")
+                numsuns = [float(i.split('_')[1]) for i in filenames]
+                means = [np.mean(np.load(f"{savepath}/{key}/{pix}/PLQE_{bias}/{i}")) for i in filenames]
+
+                means = [i for _,i in sorted(zip(numsuns, means))]
+                numsuns.sort()
+                
+                if len(numsuns) != 1:
+                    pix_ave_plqe[pix] = inter(numsuns, means, fill_value="extrapolate")(1)
+                else:
+                    pix_ave_plqe[pix] = means[0]
+
+            correction = ext_PLQE_1sun / np.mean([pix_ave_plqe[i] for i in  pix_ave_plqe.keys()])
+            print(pix_ave_plqe, ext_PLQE_1sun)
+
+
+
+            for pix in path_db[key]:
+                filenames = find_npy(f"{datapath}/{key}/{pix}/{bias}")
+                for filename in filenames:
+                    arr = np.load(f"{datapath}/{key}/{pix}/{bias}/{filename}")
+                    corrected_arr = arr * correction
+                    np.save(f"{datapath}/{key}/{pix}/{bias}/{filename}", corrected_arr)
+
+
+
+            else:
+                # We assume the file holds the oc value
+                pass # I'll do this later
+
+
+       
+
+
 
 white_buffer=[None, None, None, None]
 def callback_return_size(image_name, shape):
