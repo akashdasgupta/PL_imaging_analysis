7d47085c2ca5a99e47dfce633ae55341a3eaa6e9
Akash Dasgupta <akashdg09@gmail.com>
2023-03-04 15:24:11 +0000

    removed some functons that have been moved to measurement

--- basic_funcs.py
+++ basic_funcs.py
@@ -43,24 +43,6 @@ def path_process(path):
     return db
 
 
-def find_tif(datapath):
-    """Finds any file with the .tif extention in a path
-
-    Args:
-        datapath (str): root path to the data (with different substrates)
-
-    Returns:
-        raw_paths (list): list of filenames found ending in .tif
-    """
-    raw_paths = []
-    for _, _, files in os.walk(datapath):
-        for file in files:
-            if file.endswith(".tif"):  # Only interested in tifs
-                raw_paths.append(file)
-        break
-    return raw_paths
-
-
 def find_npy(datapath):
     """Finds any file with the .npy extention in a path
 
@@ -79,60 +61,5 @@ def find_npy(datapath):
     return raw_paths
 
 
-def get_cam_exposure(path):
-    """Pulls camera exposure time from setting dump file
-
-    Args:
-        path (str): path where camera data is located (NOT where dump file is)
-
-    Returns:
-        (float) : exposure time
-    """
-    with open(path + "/camera/camera_setting_dump.txt") as file:
-        for line in file:
-            db = eval(line)
-            db = eval(line)
-            return float(db["ExposureTime"])
 
 
-def image_intsweep_name_parser(path):
-    """Returns dict of repeat .tif data
-
-    Args:
-        path (str): Where your data lives
-
-    Returns:
-        repeat_db (dict): key = bias conditions(electrical, optical), element = list of filenames that are repeats
-    """
-    tif_names = find_tif(path)
-    repeat_db = {}
-
-    for tif_name in tif_names:
-        sm_output = tif_name.split("_")[0]
-        led_v = tif_name.split("_")[1].split("=")[1]
-
-        if f"{sm_output}_{led_v}" in repeat_db.keys():
-            repeat_db[f"{sm_output}_{led_v}"].append(tif_name)
-        else:
-            repeat_db[f"{sm_output}_{led_v}"] = [tif_name]
-    return repeat_db
-
-
-def image_for_cropping(path):
-    """Pulls up an image from the data so the interactive cropper has something to show
-
-    Args:
-        path (str): where the data lives
-
-    Returns:
-        image (numpy.ndarray): image (scaled to 8 bit) for cropping
-    """
-    image_names = find_tif(path)
-
-    nominal_vs = [i.split("_")[1].split("=")[1] for i in image_names]
-    max_filename = image_names[np.argmax(nominal_vs)]
-
-    im = np.array(Image.open(f"{path}/{max_filename}"))
-    im_scaled = im * (2 ** (8) / np.max(im))  # 8bit
-    image = im_scaled.astype(np.uint8)
-    return image
